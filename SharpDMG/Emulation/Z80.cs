22e229fab957f94ffb7b612c33a15343c3870b97
Matthew Lawson <matthew.lawson@outlook.com>
2014-05-29 16:20:04 -0400

    more opcodes implemented

--- SharpDMG/Emulation/Z80.cs
+++ SharpDMG/Emulation/Z80.cs
@@ -114,10 +114,26 @@ namespace SharpDMG.Emulation
             Console.WriteLine("Z80 core reset.");
         }
 
+        #region Misc/Control Op Functions
+
         private void NoOp()
         {
             m = 1; // One m-time taken.
         }
+
+        private void SetCarryFlag()
+        {
+            CarryFlag = true;
+            m = 1;
+        }
+
+        private void ClearCarryFlag()
+        {
+            CarryFlag = false;
+            m = 1;
+        }
+
+        #endregion
 
         #region 8-bit Load/Store Op Fuctions
         private void LoadRegisterFromRegister(ref byte to, byte from)
@@ -163,7 +179,8 @@ namespace SharpDMG.Emulation
         }
         #endregion
 
-        #region Bit level Ops
+        #region Bit level Op Functions
+
         private bool TestBit(byte subject, int bit)
         {
             return (subject & (1 << bit)) != 0;
@@ -178,6 +195,332 @@ namespace SharpDMG.Emulation
         {
             subject &= (byte)(~(1 << bit));
         }
+
+        private void ANDAddressWithRegister(ref byte to, short address)
+        {
+            byte from = cartridge.ReadByte(address);
+            to &= from;
+            ZeroFlag = to == 0;
+            SubtractionFlag = false;
+            HalfCaryFlag = true;
+            CarryFlag = true;
+            m = 2;
+        }
+
+        private void ANDRegisterWithRegister(ref byte to, byte from)
+        {
+            to &= from;
+            ZeroFlag = to == 0;
+            SubtractionFlag = false;
+            HalfCaryFlag = true;
+            CarryFlag = true;
+            m = 1;
+        }
+
+        private void ORAddressWithRegister(ref byte to, short address)
+        {
+            byte from = cartridge.ReadByte(address);
+            to |= from;
+            ZeroFlag = to == 0;
+            SubtractionFlag = false;
+            HalfCaryFlag = false;
+            CarryFlag = true;
+            m = 2;
+        }
+
+        private void ORRegisterWithRegister(ref byte to, byte from)
+        {
+            to |= from;
+            ZeroFlag = to == 0;
+            SubtractionFlag = false;
+            HalfCaryFlag = false;
+            CarryFlag = true;
+            m = 1;
+        }
+
+        private void XORAddressWithRegister(ref byte to, short address)
+        {
+            byte from = cartridge.ReadByte(address);
+            to ^= from;
+            ZeroFlag = to == 0;
+            SubtractionFlag = false;
+            HalfCaryFlag = false;
+            CarryFlag = false;
+            m = 2;
+        }
+
+        private void XORRegisterWithRegister(ref byte to, byte from)
+        {
+            to ^= from;
+            ZeroFlag = to == 0;
+            SubtractionFlag = false;
+            HalfCaryFlag = false;
+            CarryFlag = false;
+            m = 1;
+        }
+
+        #endregion
+
+        #region 8-bit Math Op Functions
+
+        //Maybe not right?  Stolen.
+        //Some emulators skip implementing this opcode anyway.
+        private void DecimallyAdjustA()
+        {
+            int highNibble = a >> 4;
+            int lowNibble = a & 0x0F;
+            bool _FC = true;
+            if (SubtractionFlag)
+            {
+                if (CarryFlag)
+                {
+                    if (HalfCaryFlag)
+                    {
+                        a += 0x9A;
+                    }
+                    else
+                    {
+                        a += 0xA0;
+                    }
+                }
+                else
+                {
+                    _FC = false;
+                    if (HalfCaryFlag)
+                    {
+                        a += 0xFA;
+                    }
+                    else
+                    {
+                        a += 0x00;
+                    }
+                }
+            }
+            else if (CarryFlag)
+            {
+                if (HalfCaryFlag || lowNibble > 9)
+                {
+                    a += 0x66;
+                }
+                else
+                {
+                    a += 0x60;
+                }
+            }
+            else if (HalfCaryFlag)
+            {
+                if (highNibble > 9)
+                {
+                    a += 0x66;
+                }
+                else
+                {
+                    a += 0x06;
+                    _FC = false;
+                }
+            }
+            else if (lowNibble > 9)
+            {
+                if (highNibble < 9)
+                {
+                    _FC = false;
+                    a += 0x06;
+                }
+                else
+                {
+                    a += 0x66;
+                }
+            }
+            else if (highNibble > 9)
+            {
+                a += 0x60;
+            }
+            else
+            {
+                _FC = false;
+            }
+
+            HalfCaryFlag = false;
+            CarryFlag = _FC;
+            ZeroFlag = a == 0;
+            m = 1;
+        }
+
+        private void IncrementRegister(ref byte register)
+        {
+            register++;
+            HalfCaryFlag = (register & 0x0F) == 0x0F;
+            SubtractionFlag = false;
+            ZeroFlag = (register == 0);
+            m = 1;
+        }
+
+        private void DecrementRegister(ref byte register)
+        {
+            register--;
+            HalfCaryFlag = (register & 0x0F) == 0x0F;
+            SubtractionFlag = true;
+            ZeroFlag = (register == 0);
+            m = 1;
+        }
+
+
+        private void IncrementAddress(short address)
+        {
+            byte data = cartridge.ReadByte(address);
+            data++;
+            HalfCaryFlag = (data & 0x0F) == 0x0F;
+            SubtractionFlag = false;
+            ZeroFlag = (data == 0);
+            cartridge.WriteByte(address, data);
+            m = 3;
+        }
+
+        private void DecrementAddress(short address)
+        {
+            byte data = cartridge.ReadByte(address);
+            data--;
+            HalfCaryFlag = (data & 0x0F) == 0x0F;
+            SubtractionFlag = true;
+            ZeroFlag = (data == 0);
+            cartridge.WriteByte(address, data);
+            m = 3;
+        }
+
+        private void ComplementRegister(ref byte register)
+        {
+            register = (byte)(~register);
+            SubtractionFlag = true;
+            HalfCaryFlag = true;
+            m = 1;
+        }
+
+        private void AddRegisterToRegister(ref byte to, byte from)
+        {
+            CarryFlag = (((int)to + (int)from) > 255);
+            to += from;
+            ZeroFlag = (to == 0);
+            HalfCaryFlag = (to & 0x0F) == 0x0F;
+            SubtractionFlag = false;
+            m = 1;
+        }
+
+        private void AddAddressToRegister(ref byte register, short address)
+        {
+            byte data = cartridge.ReadByte(address);
+            CarryFlag = (((int)register + (int)data) > 255);
+            register += data;
+            ZeroFlag = (register == 0);
+            HalfCaryFlag = (register & 0x0F) == 0x0F;
+            SubtractionFlag = false;
+            m = 2;
+        }
+
+        private void AddRegisterToRegisterWithCarry(ref byte to, byte from)
+        {
+            if (CarryFlag)
+            {
+                CarryFlag = (((int)to + (int)from + 1) > 255);                
+                to += (byte)(from + 1);
+            }
+            else
+            {
+                CarryFlag = (((int)to + (int)from + 1) > 255);
+                to += from;
+            }
+            ZeroFlag = (to == 0);
+            HalfCaryFlag = (to & 0x0F) == 0x0F;
+            SubtractionFlag = false;
+            m = 1;
+        }
+
+        private void AddAddressToRegisterWithCarry(ref byte to, short address)
+        {
+            byte from = cartridge.ReadByte(address);
+            if (CarryFlag)
+            {
+                CarryFlag = (((int)to + (int)from + 1) > 255);
+                to += (byte)(from + 1);
+            }
+            else
+            {
+                CarryFlag = (((int)to + (int)from + 1) > 255);
+                to += from;
+            }
+            ZeroFlag = (to == 0);
+            HalfCaryFlag = (to & 0x0F) == 0x0F;
+            SubtractionFlag = false;
+            m = 2;
+        }
+
+        private void SubtractRegisterFromRegister(ref byte to, byte from)
+        {
+            CarryFlag = ((int)to - (int)from) < 0;
+            to -= from;
+            ZeroFlag = to == 0;
+            SubtractionFlag = true;
+            HalfCaryFlag = (to & 0x0F) == 0x0F;
+            m = 1;
+        }
+
+        private void CompareRegister(byte to, byte from)
+        {
+            CarryFlag = ((int)to - (int)from) < 0;
+            to -= from;
+            ZeroFlag = to == 0;
+            SubtractionFlag = true;
+            HalfCaryFlag = (to & 0x0F) == 0x0F;
+            m = 1;
+        }
+
+        private void CompareAddress(byte to, short address)
+        {
+            byte from = cartridge.ReadByte(address);
+            CarryFlag = ((int)to - (int)from) < 0;
+            to -= from;
+            ZeroFlag = to == 0;
+            SubtractionFlag = true;
+            HalfCaryFlag = (to & 0x0F) == 0x0F;
+            m = 1;
+        }
+
+        private void SubtractRegisterFromRegisterWithCarry(ref byte to, byte from)
+        {
+            if (CarryFlag)
+            {
+                CarryFlag = (((int)to - (int)from - 1) < 0);
+                to -= (byte)(from + 1);
+            }
+            else
+            {
+                CarryFlag = (((int)to - (int)from - 1) < 0);
+                to -= from;
+            }
+            ZeroFlag = (to == 0);
+            HalfCaryFlag = (to & 0x0F) == 0x0F;
+            SubtractionFlag = true;
+            m = 1;
+        }
+
+        private void SubtractAddressFromRegisterWithCarry(ref byte to, short address)
+        {
+            byte from = cartridge.ReadByte(address);
+            if (CarryFlag)
+            {
+                CarryFlag = (((int)to - (int)from - 1) < 0);
+                to -= (byte)(from + 1);
+            }
+            else
+            {
+                CarryFlag = (((int)to - (int)from - 1) < 0);
+                to -= from;
+            }
+            ZeroFlag = (to == 0);
+            HalfCaryFlag = (to & 0x0F) == 0x0F;
+            SubtractionFlag = true;
+            m = 1;
+        }
+
         #endregion
 
         internal void Step()
@@ -273,91 +616,90 @@ namespace SharpDMG.Emulation
                 // 8-bit Math/Logic
 
                 // One byte ops
-                case 0x04:
-                case 0x05:
-                case 0x0C:
-                case 0x0D:
-                case 0x14:
-                case 0x15:
-                case 0x1C:
-                case 0x1D:
-                case 0x24:
-                case 0x25:
-                case 0x27:
-                case 0x2C:
-                case 0x2D:
-                case 0x2F:
-                case 0x34:
-                case 0x35:
-                case 0x37:
-                case 0x3C:
-                case 0x3D:
-                case 0x3F:
-                case 0x80:
-                case 0x81:
-                case 0x82:
-                case 0x83:
-                case 0x84:
-                case 0x85:
-                case 0x86:
-                case 0x87:
-                case 0x88:
-                case 0x89:
-                case 0x8A:
-                case 0x8B:
-                case 0x8C:
-                case 0x8D:
-                case 0x8E:
-                case 0x8F:
-                case 0x90:
-                case 0x91:
-                case 0x92:
-                case 0x93:
-                case 0x94:
-                case 0x95:
-                case 0x96:
-                case 0x97:
-                case 0x98:
-                case 0x99:
-                case 0x9A:
-                case 0x9B:
-                case 0x9C:
-                case 0x9D:
-                case 0x9E:
-                case 0x9F:
-                case 0xA0:
-                case 0xA1:
-                case 0xA2:
-                case 0xA3:
-                case 0xA4:
-                case 0xA5:
-                case 0xA6:
-                case 0xA7:
-                case 0xA8:
-                case 0xA9:
-                case 0xAA:
-                case 0xAB:
-                case 0xAC:
-                case 0xAD:
-                case 0xAE:
-                case 0xAF:
-                case 0xB0:
-                case 0xB1:
-                case 0xB2:
-                case 0xB3:
-                case 0xB4:
-                case 0xB5:
-                case 0xB6:
-                case 0xB7:
-                case 0xB8:
-                case 0xB9:
-                case 0xBA:
-                case 0xBB:
-                case 0xBC:
-                case 0xBD:
-                case 0xBE:
-                case 0xBF:
-                    break;
+                case 0x04: { IncrementRegister(ref b); break; }
+                case 0x05: { DecrementRegister(ref b); break; }
+                case 0x0C: { IncrementRegister(ref c); break; }
+                case 0x0D: { DecrementRegister(ref c); break; }
+                case 0x14: { IncrementRegister(ref d); break; }
+                case 0x15: { DecrementRegister(ref d); break; }
+                case 0x1C: { IncrementRegister(ref e); break; }
+                case 0x1D: { DecrementRegister(ref e); break; }
+                case 0x24: { IncrementRegister(ref h); break; }
+                case 0x25: { DecrementRegister(ref h); break; }
+                case 0x27: { DecimallyAdjustA(); break; }
+                case 0x2C: { IncrementRegister(ref l); break; }
+                case 0x2D: { DecrementRegister(ref l); break; }
+                case 0x2F: { ComplementRegister(ref a); break; }
+                case 0x34: { IncrementAddress(HL); break; }
+                case 0x35: { DecrementAddress(HL); break; }
+                case 0x37: { SetCarryFlag(); break; }
+                case 0x3C: { IncrementRegister(ref a); break; }
+                case 0x3D: { DecrementRegister(ref a); break; }
+                case 0x3F: { ClearCarryFlag(); break; }
+                case 0x80: { AddRegisterToRegister(ref a, b); break; }
+                case 0x81: { AddRegisterToRegister(ref a, c); break; }
+                case 0x82: { AddRegisterToRegister(ref a, d); break; }
+                case 0x83: { AddRegisterToRegister(ref a, e); break; }
+                case 0x84: { AddRegisterToRegister(ref a, h); break; }
+                case 0x85: { AddRegisterToRegister(ref a, l); break; }
+                case 0x86: { AddAddressToRegister(ref a, HL); break; }
+                case 0x87: { AddRegisterToRegister(ref a, a); break; }
+                case 0x88: { AddRegisterToRegisterWithCarry(ref a, b); break; }
+                case 0x89: { AddRegisterToRegisterWithCarry(ref a, c); break; }
+                case 0x8A: { AddRegisterToRegisterWithCarry(ref a, d); break; }
+                case 0x8B: { AddRegisterToRegisterWithCarry(ref a, e); break; }
+                case 0x8C: { AddRegisterToRegisterWithCarry(ref a, h); break; }
+                case 0x8D: { AddRegisterToRegisterWithCarry(ref a, l); break; }
+                case 0x8E: { AddAddressToRegisterWithCarry(ref a, HL); break; }
+                case 0x8F: { AddRegisterToRegisterWithCarry(ref a, a); break; }
+                case 0x90: { SubtractRegisterFromRegister(ref a, b); break; }
+                case 0x91: { SubtractRegisterFromRegister(ref a, c); break; }
+                case 0x92: { SubtractRegisterFromRegister(ref a, d); break; }
+                case 0x93: { SubtractRegisterFromRegister(ref a, e); break; }
+                case 0x94: { SubtractRegisterFromRegister(ref a, h); break; }
+                case 0x95: { SubtractRegisterFromRegister(ref a, l); break; }
+                case 0x96: { SubtractAddressFromRegisterWithCarry(ref a, HL); break; }
+                case 0x97: { SubtractRegisterFromRegister(ref a, a); break; }
+                case 0x98: { SubtractRegisterFromRegisterWithCarry(ref a, b); break; }
+                case 0x99: { SubtractRegisterFromRegisterWithCarry(ref a, c); break; }
+                case 0x9A: { SubtractRegisterFromRegisterWithCarry(ref a, d); break; }
+                case 0x9B: { SubtractRegisterFromRegisterWithCarry(ref a, e); break; }
+                case 0x9C: { SubtractRegisterFromRegisterWithCarry(ref a, h); break; }
+                case 0x9D: { SubtractRegisterFromRegisterWithCarry(ref a, l); break; }
+                case 0x9E: { SubtractAddressFromRegisterWithCarry(ref a, HL); break; }
+                case 0x9F: { SubtractRegisterFromRegisterWithCarry(ref a, a); break; }
+                case 0xA0: { ANDRegisterWithRegister(ref a, b); break; }
+                case 0xA1: { ANDRegisterWithRegister(ref a, c); break; }
+                case 0xA2: { ANDRegisterWithRegister(ref a, d); break; }
+                case 0xA3: { ANDRegisterWithRegister(ref a, e); break; }
+                case 0xA4: { ANDRegisterWithRegister(ref a, h); break; }
+                case 0xA5: { ANDRegisterWithRegister(ref a, l); break; }
+                case 0xA6: { ANDAddressWithRegister(ref a, HL); break; }
+                case 0xA7: { ANDRegisterWithRegister(ref a, a); break; }
+                case 0xA8: { XORRegisterWithRegister(ref a, b); break; }
+                case 0xA9: { XORRegisterWithRegister(ref a, c); break; }
+                case 0xAA: { XORRegisterWithRegister(ref a, d); break; }
+                case 0xAB: { XORRegisterWithRegister(ref a, e); break; }
+                case 0xAC: { XORRegisterWithRegister(ref a, h); break; }
+                case 0xAD: { XORRegisterWithRegister(ref a, l); break; }
+                case 0xAE: { XORAddressWithRegister(ref a, HL); break; }
+                case 0xAF: { XORRegisterWithRegister(ref a, a); break; }
+                case 0xB0: { ORRegisterWithRegister(ref a, b); break; }
+                case 0xB1: { ORRegisterWithRegister(ref a, c); break; }
+                case 0xB2: { ORRegisterWithRegister(ref a, d); break; }
+                case 0xB3: { ORRegisterWithRegister(ref a, e); break; }
+                case 0xB4: { ORRegisterWithRegister(ref a, h); break; }
+                case 0xB5: { ORRegisterWithRegister(ref a, l); break; }
+                case 0xB6: { ORAddressWithRegister(ref a, HL); break; }
+                case 0xB7: { ORRegisterWithRegister(ref a, a); break; }
+                case 0xB8: { CompareRegister(a, b); break; }
+                case 0xB9: { CompareRegister(a, c); break; }
+                case 0xBA: { CompareRegister(a, d); break; }
+                case 0xBB: { CompareRegister(a, e); break; }
+                case 0xBC: { CompareRegister(a, h); break; }
+                case 0xBD: { CompareRegister(a, l); break; }
+                case 0xBE: { CompareAddress(a, HL); break; }
+                case 0xBF: { CompareRegister(a, a); break; }
 
                 // Two byte ops
                 case 0xC6:
@@ -373,8 +715,8 @@ namespace SharpDMG.Emulation
                         break;
                     }
 
-                # region 8-bit Load/Store/Move
-                // 8-bit Load/Store/Move
+                # region 8-bit Load/Store
+                // 8-bit Load/Store
 
                 // Three byte ops
                 case 0xEA: { LoadMemoryFromRegister(NextWord, a); break; }
